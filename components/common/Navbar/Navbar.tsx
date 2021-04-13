781ae47934e3420e7a608ff792f6dfb3a3bbffaa
kylegalbraith <kyle.galbraith459@gmail.com>
2021-04-12 17:03:37 -0700

    rip out massive index landing page

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -15,24 +15,6 @@ const Navbar: FC = () => (
               <Logo />
             </a>
           </Link>
-          <nav className="hidden ml-6 space-x-4 lg:block">
-            <Link href="/search">
-              <a className={s.link}>All</a>
-            </Link>
-            <Link href="/search?q=clothes">
-              <a className={s.link}>Clothes</a>
-            </Link>
-            <Link href="/search?q=accessories">
-              <a className={s.link}>Accessories</a>
-            </Link>
-            <Link href="/search?q=shoes">
-              <a className={s.link}>Shoes</a>
-            </Link>
-          </nav>
-        </div>
-
-        <div className="justify-center flex-1 hidden lg:flex">
-          <Searchbar />
         </div>
 
         <div className="flex justify-end flex-1 space-x-8">
